29acba73acb612c72a521405cd433f58cb97d751
Iris Gupta <iris.gupta0@gmail.com>
2022-09-19 10:36:46 -0400

    fixed image struct

--- drawing_funcs.h
+++ drawing_funcs.h
@@ -8,12 +8,6 @@ struct Rect {
   int32_t x, y, width, height;
 };
 
-struct Image {
-  uint32_t width;
-  uint32_t height;
-  uint32_t *data;
-};
-
 void draw_pixel(struct Image *img, int32_t x, int32_t y, uint32_t color);
 
 void draw_rect(struct Image *img,
